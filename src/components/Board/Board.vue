0b35aa69100aec586ab199526989e831dbb744fb
AndreuVillaro <andreu.villaro@orkid.io>
2024-09-11 18:10:05 +0200

    Organizacion de proyecto

---
+++ src/components/Board/Board.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {},
+  },
+  data: () => ({
+
+  }),
+  mounted() {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+  },
+}
+</script>
